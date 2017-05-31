443628d7972e3ffaa05caa9650bc3ccccc65de88
maeashley <ashleystewart8888@yahoo.com>
2017-05-31 17:34:42 -0600

    im saving this

---
+++ main.js
@@ -0,0 +1,18 @@
+var wrap = require('canvas-api-wrapper')('10706~qwzHce6h66ksreAOflNk1LfTDlNlQG8RGSoktay4Jm8ffgXcxpCR56bDc1O2pvXT')
+
+wrap.callbackCall('/api/v1/courses/95/pages', null, null, function (err, pages) {
+    pages = pages.map(function (x) {
+        return x.title + '.html'
+    })
+    console.log(pages.length)
+    wrap.callbackCall('/api/v1/courses/95/files', null, null, function (err, files) {
+        files = files.map(function (x) {
+            return x.display_name
+        })
+        console.log(files.length)
+        var share = files.filter(function (x) {
+            return pages.includes(x)
+        })
+        console.log(share.length)
+    })
+})
