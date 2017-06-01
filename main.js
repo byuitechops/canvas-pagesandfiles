1dfafe800c6674c8809aa82bede11ce0e428d694
maeashley <ashleystewart8888@yahoo.com>
2017-06-01 11:09:23 -0600

    it doesn't work yet

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
+//include Ben's wrapper that allows the computer to give us all the pages or files and not just the first ten.
 var wrap = require('canvas-api-wrapper')('10706~qwzHce6h66ksreAOflNk1LfTDlNlQG8RGSoktay4Jm8ffgXcxpCR56bDc1O2pvXT')
 
+//get the pages and add a filetype
 wrap.callbackCall('/api/v1/courses/95/pages', null, null, function (err, pages) {
     pages = pages.map(function (x) {
         return x.title + '.html'
     })
+    //print out how many pages there are
     console.log(pages.length)
+
+    //get the files, filter them, and print out how many are shared between pages and files.
     wrap.callbackCall('/api/v1/courses/95/files', null, null, function (err, files) {
-        files = files.map(function (x) {
-            return x.display_name
-        })
+        /*var fileName = files.map(function (x) {return x.display_name})*/
+
         console.log(files.length)
-        var share = files.filter(function (x) {
-            return pages.includes(x)
+        var share = files.filter(function (file) {
+            //filter the objects from files to be included
+            return pages.includes(file.title)
         })
         console.log(share.length)
+        //write some stuff down because I'm trying to learn how to do this
+        function deleteFiles(share) {
+            wrap.delete('/api/v1/files/:id')
+        }
     })
 })
-//need to write a function to delete file: https://canvas.instructure.com/doc/api/files.html, scroll down to "delete file" . Will have to delete anything that's in "shared" from files.
+
+//Basically will have to empty the variable "shared" because they are all the files that we do not want.
