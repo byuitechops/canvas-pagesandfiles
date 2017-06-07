720ace662a9b76e5413761a68d0960912b4c39d4
maeashley <ashleystewart8888@yahoo.com>
2017-06-07 14:06:14 -0600

    so the function actually works.
    Don't plan on using this unless you want to delete something

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-//include Ben's wrapper that allows the computer to give us all the pages or files and not just the first ten.
-var wrap = require('canvas-api-wrapper')('10706~qwzHce6h66ksreAOflNk1LfTDlNlQG8RGSoktay4Jm8ffgXcxpCR56bDc1O2pvXT')
+/*eslint-env node*/
+/*eslint no-console:0*/
+/*global console*/
+
+//include Ben's wrapper that allows the computer to give us all the pages or files and not just the first handful that the computer can get for JSON.
+var wrap = require('canvas-api-wrapper')('10706~qwzHce6h66ksreAOflNk1LfTDlNlQG8RGSoktay4Jm8ffgXcxpCR56bDc1O2pvXT');
 
 //get the pages and add a filetype
 wrap.callbackCall('/api/v1/courses/95/pages', null, null, function (err, pages) {
@@ -7,28 +11,33 @@ wrap.callbackCall('/api/v1/courses/95/pages', null, null, function (err, pages) 
         return x.title + '.html'
     })
     //print out how many pages there are
-    console.log(pages.length)
+    console.log("PAGES", pages.length);
 
     //get the files, filter them, and print out how many are shared between pages and files.
     wrap.callbackCall('/api/v1/courses/95/files', null, null, function (err, files) {
         /*var fileName = files.map(function (x) {return x.display_name})*/
 
-        console.log(files.length)
+        console.log("FILES", files.length)
+
         var shared = files.filter(function (file) {
             //filter the objects from files that are associated with a page
             return pages.includes(file.display_name)
         })
         //log the length of the items that are duplicates
-        console.log(shared.length)
+        console.log("DUPLICATES", shared.length)
 
         deleteFiles(shared)
 
         function deleteFiles(shared) {
-            shared.forEach(function (item) {
+            shared.forEach(function (item, i) {
                 //iterate through each item in the list and delete it based on its id
-                wrap.callbackCall('https://byui.instructure.com/api/v1/files/' + shared[item].id, {}, DELETE)
+                wrap.callbackCall('/api/v1/files/' + item.id, null, 'DELETE', function (err, item) {
+                    console.log('i:', i)
+                    console.log('item', item)
+                    console.log('err:', err)
+                    console.log('------------------------------------------')
 
-                //console.log(shared.length) //double checking that the loop deleted everything. Should return 0.
+                })
             });
 
         }
